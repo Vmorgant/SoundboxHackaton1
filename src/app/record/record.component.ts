f42d6d82fc07d7f401ec458486cd3ebba3e26fc4
Unknown <victor.morgant@gmail.com>
2018-12-08 16:09:44 +0100

    record v1

--- src/app/record/record.component.ts
+++ src/app/record/record.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as RecordRTC from 'recordrtc';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-record',
@@ -6,14 +8,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./record.component.css']
 })
 export class RecordComponent implements OnInit {
-  recordMessage='Cliquez sur ce bouton pour enregistrer votre message';
-  audio = new Audio();
-  constructor() { }
+  //Lets initiate Record OBJ
+  private record;
+  //Will use this flag for detect recording
+  private recording = false;
+  //Url of Blob
+  private url;
+  private error;
+  constructor(private domSanitizer: DomSanitizer) {
+  }
+  sanitize(url:string){
+      return this.domSanitizer.bypassSecurityTrustUrl(url);
+  }
+  /**
+   * Start recording.
+   */
+  initiateRecording() {
+      
+      this.recording = true;
+      let mediaConstraints = {
+          video: false,
+          audio: true
+      };
+      navigator.mediaDevices
+          .getUserMedia(mediaConstraints)
+          .then(this.successCallback.bind(this), this.errorCallback.bind(this));
+  }
+  /**
+   * Will be called automatically.
+   */
+  successCallback(stream) {
+      var options = {
+          mimeType: "audio/wav",
+          numberOfAudioChannels: 1
+      };
+      //Start Actuall Recording
+      var StereoAudioRecorder = RecordRTC.StereoAudioRecorder;
+      this.record = new StereoAudioRecorder(stream, options);
+      this.record.record();
+  }
+  /**
+   * Stop recording.
+   */
+  stopRecording() {
+      this.recording = false;
+      this.record.stop(this.processRecording.bind(this));
+  }
+  /**
+   * processRecording Do what ever you want with blob
+   * @param  {any} blob Blog
+   */
+
+  processRecording(blob) {
+      this.url = URL.createObjectURL(blob);
+      
+  }
+  /**
+   * Process Error.
+   */
+  errorCallback(error) {
+      this.error = 'Can not play audio in your browser';
+  }
 
   ngOnInit() {
   }
-  recordStart(){
-    this.recordMessage="Enregistrement en cours";
-    
-  }
-}
+} 
