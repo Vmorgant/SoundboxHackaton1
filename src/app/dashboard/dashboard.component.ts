aef6a6ae4cee0d88cf9ce0a54d90829940c47d40
Unknown <victor.morgant@gmail.com>
2018-12-08 18:33:50 +0100

    adding new sounds

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -15,15 +15,48 @@ export class DashboardComponent {
       return  [
         {
           name: 'TG', cols: 1, rows: 1 ,
-          url: 'taGueule.ogg'
+          url: 'taGueule.ogg',
+          img: './assets/img/taGueule.png'
         },
         {
           name: 'Nani', cols: 1, rows: 1 ,
-          url: 'nani.ogg'
+          url: 'nani.ogg',
+          img: './assets/img/nani.jpg'
         },
         {
           name: 'Sabers', cols: 1, rows: 1 ,
-          url: 'lightSaber.ogg'
+          url: 'lightSaber.ogg',
+          img: './assets/img/lightSaber.jpg'
+        },
+        {
+          name: 'Boulette', cols: 1, rows: 1 ,
+          url: 'boulette.mp3',
+          img: './assets/img/boulette.jpg'
+        },
+        {
+          name: 'CestPasFaux', cols: 1, rows: 1 ,
+          url: 'cestPasFaux.m4a',
+          img: './assets/img/cestPasFaux.jpg'
+        },
+        {
+          name: 'Gigapouet', cols: 1, rows: 1 ,
+          url: 'gigapouet.mp3',
+          img: './assets/img/gigapouet.jpg'
+        },
+        {
+          name: 'Joker', cols: 1, rows: 1 ,
+          url: 'joker.mp3',
+          img: './assets/img/joker.jpg'
+        },
+        {
+          name: 'Question', cols: 1, rows: 1 ,
+          url: 'laQuestion.aac',
+          img: './assets/img/laQuestion.jpg'
+        },
+        {
+          name: 'Stephanie', cols: 1, rows: 1 ,
+          url: 'stephanie.ogg',
+          img: './assets/img/stephanie.jpg'
         },
       ];
     })
